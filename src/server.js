469d8190b336ca77426a43317474fd4d24a144ae
hieulb2432 <hieu.lb2432@gmail.com>
2022-11-04 11:10:01 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import bodyParser from "body-parser";
+import express from "express";
+import viewEngine from "./config/viewEngine";
+import connectDB from "./config/connectDB"
+import initWebRouters from "./route/web";
+require('dotenv').config()
+
+
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+initWebRouters(app);
+
+connectDB();
+
+let port = process.env.PORT;
+app.listen(port, () => {
+    console.log("Nodejs is running on port " + port)
+})
