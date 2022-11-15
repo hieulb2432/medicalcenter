741c4dd8bfe61a0748c9ce2854872f14f252f663
hieulb2432 <hieu.lb2432@gmail.com>
2022-11-15 16:51:53 +0700

    CRUD USER Update

--- Nodejs/src/services/CRUDService.js
+++ Nodejs/src/services/CRUDService.js
@@ -74,7 +74,8 @@ let updateUserData = (data) => {
     return new Promise(async(resolve, reject) => {
         try {
             let user = await db.User.findOne({
-                where: { id: data.id }
+                where: { id: data.id },
+                raw: false
             })
             if (user) {
                 user.firstName = data.firstName;
@@ -96,7 +97,8 @@ let deleteUserById = (userId) => {
     return new Promise(async(resolve, reject) => {
         try {
             let user = await db.User.findOne({
-                where: { id: userId }
+                where: { id: userId },
+                raw: false,
             })
             if (user) {
                 await user.destroy();
